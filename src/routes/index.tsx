8ac94a14dec54b69f04f02185ccfb5cd04817c66
AnJuHwan <wnghks0531@gmail.com>
2022-05-22 23:38:59 +0900

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+
+const RootRoute = () => {
+  return (
+    <Routes>
+      <Route path='/' />
+    </Routes>
+  );
+};
+
+export default RootRoute;
