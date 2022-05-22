77d4fd22cec0654d81120372c26ebb5e1c6ad52e
AnJuHwan <wnghks0531@gmail.com>
2022-05-22 23:49:51 +0900

    setting router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import RootRoute from './routes';
+
 import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <RootRoute />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <RootRoute />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
+import Page from '../pages/Page';
 
 const RootRoute = () => {
   return (
     <Routes>
-      <Route path='/' />
+      <Route path='/' element={<Page />} />
     </Routes>
   );
 };
